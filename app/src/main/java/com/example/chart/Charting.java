a3eef6a35311566af61b0974c4638f3e90a4e858
Siddharth <siddharthkumar726@gmail.com>
2023-02-16 23:12:41 +0530

    Modified with MPChart

--- app/src/main/java/com/example/chart/Charting.java
+++ app/src/main/java/com/example/chart/Charting.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
-import org.achartengine.GraphicalView;
-
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -17,29 +14,58 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendForm;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.YAxis.AxisDependency;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class Charting extends Activity {
 
-    private GraphicalView graphView;
-    private LineGraph lineGraph;
-    private LinearLayout chart;
-    int times=0;
+    private LineChart mChart;
+    private List<Entry> mEntries;
+    private int mIndex = 0;
 
     private static final String TAG = "BlueTest5-MainActivity";
+    private int mMaxChars = 50000;//Default
     private UUID mDeviceUUID;
     private BluetoothSocket mBTSocket;
     private ReadInput mReadThread = null;
@@ -48,10 +74,13 @@ public class Charting extends Activity {
     private boolean mIsUserInitiatedDisconnect = false;
 
     // All controls here
+    private TextView mTxtReceive;
     private Button mBtnClearInput;
+    private ScrollView scrollView;
+    private CheckBox chkScroll;
+    //  mTxtReceive = findViewById(R.id.txtReceive);
+//   chkScroll = findViewById(R.id.chkScroll);
     private CheckBox chkReceiveText;
-
-
     private Button disc;
 
 
@@ -67,22 +96,17 @@ public class Charting extends Activity {
         setContentView(R.layout.charting);
         ActivityHelper.initialize(this);
 
-        lineGraph = new LineGraph();
-        chart=findViewById(R.id.chart);
-        graphView = lineGraph.getView(this);
-        chart.addView(graphView);
-        chkReceiveText = findViewById(R.id.chkReceiveText);
-
-
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
         mDevice = b.getParcelable(MainActivity.DEVICE_EXTRA);
         mDeviceUUID = UUID.fromString(b.getString(MainActivity.DEVICE_UUID));
+        mMaxChars = b.getInt(MainActivity.BUFFER_SIZE);
         Log.d(TAG, "Ready");
-        mBtnClearInput = findViewById(R.id.Clear);
+        //   mTxtReceive.setMovementMethod(new ScrollingMovementMethod());
         disc=findViewById(R.id.disc);
+        chkReceiveText=findViewById(R.id.chkReceiveText);
 
-        mBtnClearInput.setOnClickListener(stopInput());
+     //   mBtnClearInput.setOnClickListener(arg0 -> mTxtReceive.setText(""));
 
         disc.setOnClickListener(view -> {
 
@@ -102,12 +126,64 @@ public class Charting extends Activity {
         });
 
 
-    }
+        //for charting purpose
 
-    private View.OnClickListener stopInput() {
-        times=0;
-        graphView.repaint();
-        return null;
+        mChart = findViewById(R.id.chart1);
+        mEntries = new ArrayList<>();
+
+        // Disable description text
+        mChart.getDescription().setEnabled(false);
+
+
+        // Enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // Enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+
+        // If disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // Set the background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        // Add an empty data set to the chart
+        LineData data = new LineData();
+        mChart.setData(data);
+
+        // Customize x-axis
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // only intervals of 1 unit
+        xAxis.setLabelCount(5); // show 5 labels
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                // Convert float to int to display as label
+                return String.valueOf((int) value);
+            }
+        });
+
+        // Customize y-axis
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(120f);
+
+
+        //removable for right axis
+        YAxis rightAxis=mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        //removable for right axis
+
+        // Add empty data to the chart
+        addEntry(0);
+        //for charting purpose
+
+
     }
 
     private class ReadInput implements Runnable {
@@ -135,7 +211,6 @@ public class Charting extends Activity {
                     if (inputStream.available() > 0) {
                         inputStream.read(buffer);
                         int i = 0;
-                        times+=1;
                         /*
                          * This is needed because new String(buffer) is taking the entire buffer i.e. 256 chars on Android 2.3.4 http://stackoverflow.com/a/8843462/1287554
                          */
@@ -143,22 +218,25 @@ public class Charting extends Activity {
                         }
                         final String strInput = new String(buffer, 0, i);
                         int gsrV=extractNumberFromString(strInput);
+
                         /*
                          * If checked then receive text, better design would probably be to stop thread if unchecked and free resources, but this is a quick fix
                          */
 
                         if (chkReceiveText.isChecked()) {
-                        //    mTxtReceive.post(() -> {
-                             //   mTxtReceive.append(strInput);
+                             //  mTxtReceive.post(() -> {
+                              //  mTxtReceive.append(strInput);
+                             //  });
 
-                                lineGraph.addNewPoints(Point.Pointset(times,gsrV));
-                                graphView.repaint();
+                            addEntry(new Random().nextInt(100));
 
-                        //    });
+
+
+
                         }
 
                     }
-                    Thread.sleep(2000);
+                    Thread.sleep(500);
                 }
             } catch (IOException e) {
 // TODO Auto-generated catch block
@@ -169,6 +247,7 @@ public class Charting extends Activity {
             }
 
         }
+
 
         private int extractNumberFromString(String strInput) {
             int number=0;
@@ -327,6 +406,68 @@ public class Charting extends Activity {
 
             progressDialog.dismiss();
         }
+
+    }
+
+    private ILineDataSet createSet() {
+        LineDataSet set = new LineDataSet(mEntries, "Real-time Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.RED);
+        set.setLineWidth(2f);
+        set.setCircleSize(4f);
+        set.setDrawCircles(true);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        return set;
+    }
+
+    private void addEntry(int value) {
+        // Add new data to the chart
+        //   mEntries.add(new Entry(mIndex, value));
+        //   mIndex++;
+
+        //removable
+
+        if(mEntries.size()>0 && mEntries.get(mEntries.size()-1).getX()==mIndex){
+            mEntries.set(mEntries.size()-1,new Entry(mIndex,value));
+        }
+        else
+        {
+            mEntries.add(new Entry(mIndex,value));
+        }
+
+        mIndex++;
+        //removable
+
+        // Update the data set
+        LineData data = mChart.getData();
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+            if (set == null) {
+                set =createSet();
+                data.addDataSet(set);
+            }
+            data.addEntry(new Entry(mIndex, value), 0);
+            data.notifyDataChanged();
+            mChart.notifyDataSetChanged();
+            mChart.setVisibleXRangeMaximum(10); // display only the last 10 entries
+            // Move the chart to the latest entry
+            mChart.moveViewToX(mEntries.size() - 1);
+
+        }
+
+        //    private LineDataSet createSet(){
+        //        LineDataSet set = new LineDataSet(mEntries, "Real-time Data");
+        //        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        //        set.setColor(Color.RED);
+        //        set.setLineWidth(2f);
+        //        set.setDrawCircles(false);
+        //        set.setDrawValues(false);
+        //       return set;
+
+
+        // }
 
     }
 
